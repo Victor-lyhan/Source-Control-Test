20477fe852501b92ff1cf87a705f3ff5a944f178
Victor Han <liyanghan1112@gmail.com>
2022-07-21 14:21:23 -0500

    Initial Commit

---
+++ Source Control Test/Source_Control_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_Control_TestApp.swift
+//  Source Control Test
+//
+//  Created by Victor Han on 2022/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Source_Control_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
